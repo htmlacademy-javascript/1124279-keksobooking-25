c2868198f13e3fc439335a57225a2629d898fd3e
Ivan Podorozhnyi <ivanpodorozhnj@gmail.com>
2022-02-23 17:24:52 +0200

    create two functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-function formattingRange(min, max) {
+// function formattingRange(min, max) {
+//   if (min === max) {
+//   min = 0;
+//   }
+//   if (min < 0 || max < 0) {
+//   min = Math.abs(min);
+//   max = Math.abs(max);
+//   }
+//   if (min > max) {
+//   [min, max] = [max, min];
+//   }
+//   return [min, max];
+//   }
+
+//   function getRandomNumber(min, max) {
+//   const arr = formattingRange(min, max);
+//   return Math.floor(Math.random() * (arr[1] - arr[0] + 1) + arr[0]);
+//   }
+
+//   function generateRandomFloat(min, max, decimaPoint) {
+//   const arr = formattingRange(min, max);
+//   return (Math.random() * (arr[1] - arr[0] + 1) + arr[0]).toFixed(decimaPoint);
+//   }
+
+//   generateRandomFloat(7, -5, 3);
+//   getRandomNumber(-121, -6);
+
+function getRandomNumber(min, max) {
   if (min === max) {
     min = 0;
   }
@@ -9,21 +36,24 @@ function formattingRange(min, max) {
   if (min > max) {
     [min, max] = [max, min];
   }
-  return [min, max];
-}
 
-function getRandomNumber(min, max) {
-  const arr = formattingRange(min, max);
-  return Math.floor(Math.random() * (arr[1] - arr[0] + 1) + arr[0]);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function generateRandomFloat(min, max, decimaPoint) {
-  const arr = formattingRange(min, max);
-  return (Math.random() * (arr[1] - arr[0] + 1) + arr[0]).toFixed(decimaPoint);
+  if (min === max) {
+    min = 0;
+  }
+  if (min < 0 || max < 0) {
+    min = Math.abs(min);
+    max = Math.abs(max);
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return (Math.random() * (max - min + 1) + min).toFixed(decimaPoint);
 }
 
 
 generateRandomFloat(7, -5, 3);
 getRandomNumber(-121, -6);
-
-
