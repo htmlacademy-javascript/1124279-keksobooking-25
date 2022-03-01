23397c94aabc26cadf1d8cc988e0a75d42cb4070
Ivan Podorozhnyi <ivanpodorozhnj@gmail.com>
2022-03-01 17:51:19 +0200

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function generateRandomFloat(a, b, digits = 1) {
   const upper = Math.max(Math.abs(a), Math.abs(b));
 
   const result = Math.random() * (upper - lower) + lower;
-  return +result.toFixed(digits);
+  return Number(result.toFixed(digits));
 }
 
 const formattingNumber = (arr) => {
@@ -55,39 +55,50 @@ const getRandomArray = (elements) => {
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 
-const TYPE_HOUSTING = ['palace', 'flat', 'house', 'bungalow', 'hotel'],
-  CHECKIN = ['12:00', '13:00', '14:00'],
-  CHECOUT = ['12:00', '13:00', '14:00'],
-  FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  DESCRIPTION = ['Etiam finibus magna', 'et est aliquam, sed semper libero facilisis', 'Donec lectus lorem', 'rhoncus vitae quam eget', 'vulputate gravida elit', 'Praesent ultricieseros id velit condimentum', 'eu ultrices nisl consequat'],
-  PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const TYPES_HOUSTING = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+const CHECOUT_TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = ['Etiam finibus magna', 'et est aliquam, sed semper libero facilisis', 'Donec lectus lorem','rhoncus vitae quam eget', 'vulputate gravida elit','Praesent ultricieseros id velit condimentum','eu ultrices nisl consequat'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const LOCATION_LAT = {
+  min: 35.65000,
+  max: 35.70000
+};
+const LOCATION_LNG = {
+  min: 139.70000,
+  max: 139.80000
+};
+const FLOATING_NUMBER = 5;
+const LENGTH_ARRAY_ADS = 10;
+const MIN_VALUE = 1;
+const MAX_PRICE = 10000;
+const MAX_NUMBER_ROOMS = 3;
+const MAX_GUEST = 7;
 
-
-const formatNumbers = formattingNumber(createNotRepeatNumbers(1, 10));
-
-
+const arrayFormattedNumbers = formattingNumber(createNotRepeatNumbers(1, 10));
 const similarAds = Array.from({
-  length: 10
+  length: LENGTH_ARRAY_ADS
 }, (_value, index) => {
   const location = {
-    lat: generateRandomFloat(35.65000, 35.70000, 5),
-    lng: generateRandomFloat(139.70000, 139.80000, 5),
-  }
+    lat: generateRandomFloat(LOCATION_LAT.min, LOCATION_LAT.max, FLOATING_NUMBER),
+    lng: generateRandomFloat(LOCATION_LNG.min, LOCATION_LNG.max, FLOATING_NUMBER),
+  };
   return {
     author: {
-      avatar: `img/avatars/user${formatNumbers[index]}.png`,
+      avatar: `img/avatars/user${arrayFormattedNumbers[index]}.png`,
     },
     offer: {
-      title: `Предложения №${formatNumbers[index]}`,
+      title: `Предложения №${arrayFormattedNumbers[index]}`,
       address: `${location.lat}, ${location.lng}`,
-      price: getRandomNumber(1, 10000),
-      type: getRandomArrayElement(TYPE_HOUSTING),
-      rooms: getRandomNumber(1, 3),
-      guests: getRandomNumber(1, 7),
-      checkin: getRandomArrayElement(CHECKIN),
-      checkout: getRandomArrayElement(CHECOUT),
+      price: getRandomNumber(MIN_VALUE, MAX_PRICE),
+      type: getRandomArrayElement(TYPES_HOUSTING),
+      rooms: getRandomNumber(MIN_VALUE, MAX_NUMBER_ROOMS),
+      guests: getRandomNumber(MIN_VALUE, MAX_GUEST),
+      checkin: getRandomArrayElement(CHECKIN_TIMES),
+      checkout: getRandomArrayElement(CHECOUT_TIMES),
       features: getRandomArray(FEATURES),
-      description: getRandomArrayElement(DESCRIPTION),
+      description: getRandomArrayElement(DESCRIPTIONS),
       photos: getRandomArray(PHOTOS),
     },
     location: {
