d9b1afd2058aa514352227e75289423cd2297c3e
Keks <keks@htmlacademy.ru>
2022-03-15 09:29:32 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+import './data.js';
+import './util.js';
+import './generating-similar-ads.js';
+import './form-status.js';
+
+import {setInactiveForm, setActriveForm} from './form-status.js';
+
+setInactiveForm();
+// setActriveForm();
