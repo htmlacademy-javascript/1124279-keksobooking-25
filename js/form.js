cac9b285bafb9d481e3170922d753e1236d4d500
Ivan Podorozhnyi <ivanpodorozhnj@gmail.com>
2022-03-20 13:12:12 +0200

    8.14. Правда или действие (часть 2)

--- js/form.js
+++ js/form.js
@@ -1,6 +1,10 @@
 const adForm = document.querySelector('.ad-form');
 const roomNumber = adForm.querySelector('#room_number');
 const copacity = adForm.querySelector('#capacity');
+const typeOfHousting = adForm.querySelector('#type');
+const price = adForm.querySelector('#price');
+const checkin = adForm.querySelector('#timein');
+const checkout = adForm.querySelector('#timeout');
 
 
 const roomCopacity = {
@@ -8,6 +12,14 @@ const roomCopacity = {
   '2' : ['1', '2'],
   '3' : ['1', '2','3'],
   '100' : ['0'],
+};
+
+const priceHousting = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
 };
 
 const pristine = new Pristine(adForm, {
@@ -24,6 +36,30 @@ pristine.addValidator(roomNumber, copacityValidation, 'Не допустимый
 
 copacity.addEventListener('change', () => {
   pristine.validate(roomNumber);
+});
+
+function priceValidator() {
+  return +price.value >= +price.min;
+}
+
+function priceErrorMessage () {
+  return `Цена за ночь не может привышать ${price.min}`;
+}
+
+pristine.addValidator(price, priceValidator, priceErrorMessage);
+
+typeOfHousting.addEventListener('change', () => {
+  price.placeholder = priceHousting[typeOfHousting.value];
+  price.min = priceHousting[typeOfHousting.value];
+  pristine.validate(price);
+});
+
+checkin.addEventListener('change', (evt) => {
+  checkout.value = evt.target.value;
+});
+
+checkout.addEventListener('change', (evt) => {
+  checkin.value = evt.target.value;
 });
 
 adForm.addEventListener('submit', (evt) => {
