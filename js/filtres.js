b886f27b0306dd2cfce76e924979aaf33ab7a1d4
Ivan Podorozhnyi <ivanpodorozhnj@gmail.com>
2022-04-01 18:52:36 +0300

    Перламутровые пуговицы

--- js/filtres.js
+++ js/filtres.js
@@ -1,84 +1,76 @@
-
-
 const housingType = document.querySelector('#housing-type');
 const housingPrice = document.querySelector('#housing-price');
 const housingRooms = document.querySelector('#housing-rooms');
 const housingGuest = document.querySelector('#housing-guests');
 
+const MIN_COST_VALUE = 10000;
+const MAX_COST_VALUE = 50000;
+
 
 function sortHousingType(point) {
-  if (housingType.value === 'any') {return true;}
+  if (housingType.value === 'any') {
+    return true;
+  }
   return point.offer.type === housingType.value;
 }
 
 function sortHousingRooms(point) {
-  if (housingRooms.value === 'any') {return true;}
-  return Number(housingRooms.value)  === Number(point.offer.rooms);
+  if (housingRooms.value === 'any') {
+    return true;
+  }
+  return Number(housingRooms.value) === Number(point.offer.rooms);
 }
 
 function sortHousingGuest(point) {
-  if (housingGuest.value === 'any') {return true;}
-  return Number(housingGuest.value)  === Number(point.offer.guests);
+  if (housingGuest.value === 'any') {
+    return true;
+  }
+  return Number(housingGuest.value) === Number(point.offer.guests);
 }
 
 function sortHousingPrice(point) {
-  if (housingPrice.value === 'any') {return true;}
-  if (housingPrice.value === 'low') {
-    if (point.offer.price < 10000) {
-      return -1;
-    }
+  switch (housingPrice.value) {
+    case 'any':
+      return true;
+    case 'low':
+      if (point.offer.price < MIN_COST_VALUE) {
+        return -1;
+      }
+      break;
+    case 'middle':
+      if (point.offer.price >= MIN_COST_VALUE && point.offer.price <= MAX_COST_VALUE) {
+        return -1;
+      }
+      break;
+    case 'high':
+      if (point.offer.price >= MAX_COST_VALUE) {
+        return -1;
+      }
+      break;
   }
-  if (housingPrice.value === 'middle') {
-    if (point.offer.price >= 10000 && point.offer.price <= 50000) {
-      return -1;
-    }
-
-  }
-  if (housingPrice.value === 'high') {
-    if (point.offer.price >= 50000) {
-      return -1;
-    }
-  }
 }
 
-function setHousingRooms(cb) {
-  housingRooms.addEventListener('change', () => {
-    cb();
-  });
-}
-
-function setCostHousing(cb) {
-  housingPrice.addEventListener('change', () => {
-    cb();
-  });
-}
-function setHousingGuest(cb) {
-  housingGuest.addEventListener('change', () => {
-    cb();
-  });
-}
-
-function setTypeHousing(cb) {
-  housingType.addEventListener('change', () => {
+function setFilterChange(item, cb) {
+  item.addEventListener('change', () => {
     cb();
   });
 }
 
 
-function resetFilter () {
+
+function resetFilter() {
   housingType.value = 'any';
   housingPrice.value = 'any';
   housingRooms.value = 'any';
   housingGuest.value = 'any';
 }
 export {
-  resetFilter,
-  setHousingGuest,
   sortHousingGuest,
-  setHousingRooms,
+  resetFilter,
+  setFilterChange,
   sortHousingRooms,
   sortHousingPrice,
   sortHousingType,
-  setTypeHousing,
-  setCostHousing
+
+
 };
--- js/main.js
+++ js/main.js
@@ -8,15 +8,19 @@ import './api.js';
 import {createMarker} from './map.js';
 import {setFormSubmit} from './form.js';
 import {getData} from './api.js';
-import {setTypeHousing, setHousingRooms, setCostHousing, setHousingGuest} from './filtres.js';
+import {setFilterChange} from './filtres.js';
 const SIMMILAR_ADS_COUNT = 10;
 
+const housingType = document.querySelector('#housing-type');
+const housingPrice = document.querySelector('#housing-price');
+const housingRooms = document.querySelector('#housing-rooms');
+const housingGuest = document.querySelector('#housing-guests');
 
 getData((ads) => {
   createMarker(ads, SIMMILAR_ADS_COUNT);
-  setTypeHousing(() => createMarker(ads, SIMMILAR_ADS_COUNT));
-  setCostHousing(() => createMarker(ads, SIMMILAR_ADS_COUNT));
-  setHousingRooms(() => createMarker(ads, SIMMILAR_ADS_COUNT));
-  setHousingGuest(() => createMarker(ads, SIMMILAR_ADS_COUNT));
+  setFilterChange(housingGuest,() => createMarker(ads, SIMMILAR_ADS_COUNT));
+  setFilterChange(housingRooms, () => createMarker(ads, SIMMILAR_ADS_COUNT));
+  setFilterChange(housingPrice, () => createMarker(ads, SIMMILAR_ADS_COUNT));
+  setFilterChange(housingType, () => createMarker(ads, SIMMILAR_ADS_COUNT));
 });
 setFormSubmit();
