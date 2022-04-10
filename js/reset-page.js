e12c9038a792a48217d163ef8162dbdb1a042343
Ivan Podorozhnyi <ivanpodorozhnj@gmail.com>
2022-04-10 19:44:18 +0300

    Правки перед защитой

--- js/reset-page.js
+++ js/reset-page.js
@@ -1,20 +1,22 @@
 import {
   resetMainPin,
   closePopup,
-  removePins
+  removePins,
+  createMarker
 } from './map.js';
 import {
   setAddressValue
 } from './form-status.js';
 import {resetFilter} from './filtres.js';
 import { getData } from './api.js';
-import { createMarker } from './map.js';
 import {resetImage} from './images.js';
+import {resetValueForm} from './form.js';
 
 function resetPage(form) {
   form.reset();
   setAddressValue();
   resetMainPin();
+  resetValueForm();
   closePopup();
   resetFilter();
   removePins();
